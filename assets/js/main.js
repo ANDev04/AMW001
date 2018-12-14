ba68c55c0a2fbfbe8eadcde240f55cc04519acf8
iChwn <ichwanarif26@gmail.com>
2018-12-14 09:43:39 +0700

    Add Semicolon Sample to code, Bwahahaha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,29 @@
-const startBtn = document.querySelector("#btnStart")
+const startBtn = document.querySelector("#btnStart");
 const AMW001 = {
+  checkCondition : false,
   init: function() {
     startBtn.addEventListener("click", function() {
-      AMW001.start()
-    })
+      if (AMW001.checkCondition == false) {
+        AMW001.start();
+      } else {
+        AMW001.pause();
+      }
+    });
   },
   start: function() {
+    AMW001.checkCondition = true;
     let animation = anime({
       targets: "#btnStart #btnStartIcon",
       scale: 0.5,
       opacity: 0,
       easing: "easeInOutBack"
-    })
+    });
   },
   pause: function() {
-    console.log("Hello Ini Pause")
+    AMW001.checkCondition = false;
+    console.log("Hello Ini Pause");
   },
   stop: function() {
-    console.log("Hello Ini Stop")
+    console.log("Hello Ini Stop");
   }
-}
+};
