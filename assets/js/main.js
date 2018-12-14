2cd3e2a7599468ac689afb1c8e225e02317d8e8e
Adityawan Chandra <adityawancha@gmail.com>
2018-12-14 10:47:20 +0700

    Add pause event

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,30 @@
-const startBtn = document.querySelector("#btnStart");
+const startBtn = document.querySelector("#btnStart")
 const AMW001 = {
-  checkCondition : false,
+  isPlay: false,
+  animation: null,
   init: function() {
-    startBtn.addEventListener("click", function() {
-      if (AMW001.checkCondition == false) {
-        AMW001.start();
-      } else {
-        AMW001.pause();
-      }
-    });
-  },
-  start: function() {
-    AMW001.checkCondition = true;
-    let animation = anime({
+    // Init Animasi Timeline
+    AMW001.animation = anime.timeline({ autoplay: false })
+    AMW001.animation.add({
       targets: "#btnStart #btnStartIcon",
       scale: 0.5,
       opacity: 0,
       easing: "easeInOutBack"
-    });
+    })
+
+    startBtn.addEventListener("click", function() {
+      AMW001.isPlay ? AMW001.pause() : AMW001.start()
+    })
+  },
+  start: function() {
+    AMW001.isPlay = true
+    AMW001.animation.play()
   },
   pause: function() {
-    AMW001.checkCondition = false;
-    console.log("Hello Ini Pause");
+    AMW001.animation.pause()
+    AMW001.isPlay = false
   },
   stop: function() {
-    console.log("Hello Ini Stop");
+    // Stop Animasi dan mengulang dari awal
   }
-};
+}
