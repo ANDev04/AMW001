14f04b414ba33145aa3718e616dd79e5177356bb
Adityawan Chandra <adityawancha@gmail.com>
2018-12-14 11:12:47 +0700

    Add + Stop Event + Restart Event + Control using Key

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 const startBtn = document.querySelector("#btnStart")
 const AMW001 = {
   isPlay: false,
+  currentDuration: 0,
   animation: null,
   init: function() {
     // Init Animasi Timeline
-    AMW001.animation = anime.timeline({ autoplay: false })
+    AMW001.animation = anime.timeline({
+      autoplay: false,
+      update: function(anim) {
+        AMW001.currentDuration = anim.progress
+      }
+    })
     AMW001.animation.add({
       targets: "#btnStart #btnStartIcon",
       scale: 0.5,
@@ -15,6 +21,13 @@ const AMW001 = {
     startBtn.addEventListener("click", function() {
       AMW001.isPlay ? AMW001.pause() : AMW001.start()
     })
+
+    document.addEventListener("keydown", function(e) {
+      if (e.key === "z" || e.key === "Z") AMW001.start()
+      if (e.key === "x" || e.key === "X") AMW001.pause()
+      if (e.key === "c" || e.key === "C") AMW001.stop()
+      if (e.key === "v" || e.key === "V") AMW001.restart()
+    })
   },
   start: function() {
     AMW001.isPlay = true
@@ -24,7 +37,17 @@ const AMW001 = {
     AMW001.animation.pause()
     AMW001.isPlay = false
   },
+  restart: function() {
+    // Mengulang dari awal
+    if (AMW001.currentDuration !== 0) {
+      AMW001.animation.restart()
+    }
+  },
   stop: function() {
     // Stop Animasi dan mengulang dari awal
+    if (AMW001.currentDuration !== 0) {
+      AMW001.animation.pause()
+      AMW001.animation.seek(0)
+    }
   }
 }
